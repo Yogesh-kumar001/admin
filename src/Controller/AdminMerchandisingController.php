6bcb23bd4a9eaf0fa4bee0d1588589119ea4d3b4
Yogesh-kumar001 <yogesh@vsteq.in>
2025-06-24 14:00:37 +0530

    Update AdminMerchandisingController.php

--- src/Controller/AdminMerchandisingController.php
+++ src/Controller/AdminMerchandisingController.php
@@ -16,23 +16,4 @@ class AdminMerchandisingController extends FrameworkBundleAdminController
 }
 
 
-// namespace Awvisualmerchandising\Controller;
 
-// use Doctrine\Common\Cache\CacheProvider;
-// use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
-// use Symfony\Component\HttpFoundation\Response;
-
-// class AdminMerchandisingController extends FrameworkBundleAdminController
-// {
-//     private $cache;
-
-//     public function __construct(CacheProvider $cache)
-//     {
-//         $this->cache = $cache;
-//     }
-
-//     public function indexAction()
-//     {
-//         return $this->render('@Modules/awvisualmerchandising/views/templates/admin/index.html.twig');
-//     }
-// }
